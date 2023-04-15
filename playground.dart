635c847c6029b25d5ee08e5ca7d86dce66d8b273
Evoque <99081397+MaqC254@users.noreply.github.com>
2023-04-15 10:48:27 +0300

    Add files via upload
    Day 2 of learning dart

--- playground.dart
+++ playground.dart
@@ -1,38 +1,32 @@
-import 'dart:io';
-import 'dart:mirrors';
 main() {
-  print('Please input your name.');
-  var name = stdin.readLineSync();
-  stdout.writeln('Your name is $name');
-  //Three foward slash is documentation
-  // int
-  // double
-  // bool 
-  // String
-  // dynamic
-  //escape
-  var a = 'hello it\'s me';
-  //raw string = string that does not support newline eg:
-  var b = r'This is a new \n rawstring';
-  stdout.writeln('The string is $b');
-  //multiline string uses '''String'''
-//Conversion
-//String to int
-var one = int.parse('1');
-assert(one == 1);
+  //Null aware operators
+  //(?.)if not null then work, (??)num ?? 0 if null then the value is 0, (??=)it is used to assign if it is null  
+  var n = Num();
+  int number;
+  if (n != null){
+    number = n.num;
+  }
 
-//String to double
-var onePointOne = double.parse('1.1');
-assert(onePointOne == 1.1);
-// int to String
-String oneAsString = 1.toString();
+  //Ternary ops
+  int x = 300;
+  var result = x%2 == 0 ? 'Even':'Odd';
+  print(result);
 
-// double to String
-String piToString = 3.142.toStringAsFixed(2);//to 3.14 as a String
-const PI = 3.142;
-print(PI.runtimeType);
+  //Type test
+  var z = 100;
+  if(x is int){
+    print('This is an integer');
+  }
+//Compare ops and loops same as Java
+//for in loop;
+var y = [1,2,3,4,5];
+for (var n in y){
+  print (n);
+}
+//for each loop
+y.forEach((n)=> print(n));
+}
 
-
-
-  
+class Num{
+  int num = 10;
 }
